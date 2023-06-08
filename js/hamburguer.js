ecc2ac410137412d3315bc3ab28e4dbcae595c5c
alex <raulalexo100@gmail.com>
2023-06-07 17:24:27 -0700

    Add animation for nav links

--- js/hamburguer.js
+++ js/hamburguer.js
@@ -1,3 +1,4 @@
 document.getElementById('hamburguer').addEventListener('click', () => {
     document.getElementById('hamburguer').classList.toggle('toggled');
+    document.getElementById('nav-links').classList.toggle('visible');
 });
