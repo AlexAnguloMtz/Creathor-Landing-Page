89b4cd5d04f9d51728d22c9fc4c968d7924c74fb
alex <raulalexo100@gmail.com>
2023-06-09 16:18:25 -0700

    Add first image to about us section

--- js/about-us.js
+++ js/about-us.js
@@ -1,20 +1,25 @@
 import { createElement } from './document-utils.js';
 
+const imagesBasePath = '../assets/images/';
+
 const cards = [
     {
         title: 'Programación móvil',
         text: `Creamos apps de alto rendimiento para iOS y Android, brindando una experiencia fluida y
-        funcional a tus usuarios.`
+        funcional a tus usuarios.`,
+        img: { src: `${imagesBasePath}mobile.jpg` }
     },
     {
         title: 'Diseño UI / UX',
         text: `Diseñamos interfaces intuitivas y atractivas, optimizando la usabilidad y experiencia del
-        usuario en tu sitio web o aplicación.`
+        usuario en tu sitio web o aplicación.`,
+        img: { src: '' }
     },
     {
         title: 'Posicionamiento web',
         text: `Mejoramos tu visibilidad en buscadores con SEO efectivo, atrayendo tráfico cualificado para
-        maximizar tu impacto en línea.`
+        maximizar tu impacto en línea.`,
+        img: { src: '' }
     }
 ]
 
@@ -42,7 +47,7 @@ function appendCard(card) {
 
     const img = createElement({
         tag: 'img',
-        attributes: [{ name: 'src', value: '' }]
+        attributes: [{ name: 'src', value: card.img.src }]
     });
 
     const cardElement = createElement({
