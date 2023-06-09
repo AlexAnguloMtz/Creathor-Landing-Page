71687b5457aaf2a75cd82c09078c2715163e186f
alex <raulalexo100@gmail.com>
2023-06-09 03:39:08 -0700

    Fix some layout issues with carousel

--- js/about-us.js
+++ js/about-us.js
@@ -21,7 +21,6 @@ cards.forEach(appendCard);
 function appendCard(card) {
 
     const cardTitle = createElement({ tag: 'h1', classes: ['title'], children: [card.title] });
-
     const cardText = createElement({ tag: 'p', classes: ['text'], children: [card.text] });
 
     const cardBody = createElement({ tag: 'div', classes: ['body'], children: [cardTitle, cardText] });
@@ -29,7 +28,6 @@ function appendCard(card) {
     const img = createElement({ tag: 'img', attributes: [{ name: 'src', value: '' }] });
 
     const cardElement = createElement({ tag: 'article', classes: ['card'], children: [img, cardBody] });
-
     document.querySelector('.about-us .cards').append(cardElement);
 
 }
